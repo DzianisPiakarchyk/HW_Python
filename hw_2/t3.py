70f9fe9295268b6e28be0bd79f9dcbb7fcb81ddf
DzianisPiakarchyk <120118441+DzianisPiakarchyk@users.noreply.github.com>
2023-02-24 02:02:26 +0300

    Update t3.py

--- hw_2/t3.py
+++ hw_2/t3.py
@@ -5,10 +5,24 @@ os.system('cls')
 # (т.е. числа вида 2k), не превосходящие числа N.
 
 k = int(input("Введите число: "))
-if k < 1:
-    print('Натуральное число!')
-else:
-    i = 0
-    while 2 ** i <= k:
-        print(2 ** i)
-        i += 1
+
+# if k < 1:
+#     print('Натуральное число!')
+# else:
+#     i = 0
+#     while 2 ** i <= k:
+#         print(2 ** i)
+#         i += 1
+        
+# lst1 = [2**i for i in range (k) if 2**i <= k]
+# print(lst1)
+
+# lst2 = [2**i for i in range (1, k) if 2**i <= k]
+# print(lst2)
+
+step = []
+i = 0
+while 2**i <= k:
+    step.append(2**i)
+    i += 1
+print(f'{k} --> {step}')
