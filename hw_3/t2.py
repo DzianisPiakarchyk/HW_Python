6d07315c20e3c69543bec16eb7be757132e35c5e
DzianisPiakarchyk <120118441+DzianisPiakarchyk@users.noreply.github.com>
2023-02-26 12:19:04 +0300

    Update t2.py

--- hw_3/t2.py
+++ hw_3/t2.py
@@ -8,8 +8,9 @@ print(arr)
 X = int(input("Введите проверяемое число: "))
 min_diff = abs(arr[0] - X)
 closest_num = arr[0]
-for i in range(1, n):
-    if abs(arr[i] - X) < min_diff:
-        min_diff = abs(arr[i] - X)
+for i in arr:
+#     if abs(arr[i] - X) < min_diff:
+#         min_diff = abs(arr[i] - X)
+      if min_diff > (min_diff := abs(arr[i] - X)):
         closest_num = arr[i]
 print(closest_num)
