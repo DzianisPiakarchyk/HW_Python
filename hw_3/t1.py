2bbae18cb64f20cf0985a5eff0d24497865611c1
DzianisPiakarchyk <120118441+DzianisPiakarchyk@users.noreply.github.com>
2023-02-26 12:06:56 +0300

    Update t1.py

--- hw_3/t1.py
+++ hw_3/t1.py
@@ -7,7 +7,7 @@ arr = [random.randint(0, 6) for _ in range(n)]
 print(arr)
 X = int(input("Введите проверяемое число: "))
 count = 0
-for i in range(n):
-    if arr[i] == X:
+for i in range(n):     # for i in arr:
+    if arr[i] == X:    # if i == X:
         count += 1
 print(f'Число {X} встречается в списке вот столько раз: {count}.')
