527a92cadcdffeca34697a8324830360c77b25d1
DzianisPiakarchyk <120118441+DzianisPiakarchyk@users.noreply.github.com>
2023-03-05 23:57:40 +0300

    Update t2.py

--- hw_5/t2.py
+++ hw_5/t2.py
@@ -9,7 +9,6 @@ b = int(input("Введите второе слагаемое: "))
 def sum(a, b):
     if b == 0:
         return a
-    else:
-        return sum(a + 1, b - 1)
+    return sum(a + 1, b - 1)
 
 print(sum(a, b))
