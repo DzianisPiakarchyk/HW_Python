ff6c301631b82042be61f9f4d0de561aff8021b5
DzianisPiakarchyk <120118441+DzianisPiakarchyk@users.noreply.github.com>
2023-03-13 01:10:05 +0300

    Update t1.py

--- hw_7/t1.py
+++ hw_7/t1.py
@@ -3,18 +3,18 @@ os.system('cls')
 
 
 
-def ritm_slogi(stihi):
-    razdelno = stihi.split()
-    glasnye = []
-    for slovo in razdelno:
-        glasnye_odinak = len([i for i in slovo if i.lower() in 'ёуеыаоэяию'])
-        glasnye.append(glasnye_odinak)
-    if len(set(glasnye)) == 1:
+def rhythm(poems):
+    separated = poems.lower().split()
+    vowels = []
+    for word in separated:
+        same_vowels = len([i for i in word if i in 'ёуеыаоэяию'])
+        vowels.append(same_vowels)
+    if len(set(vowels)) == 1:
         return "Ритм есть. Парам пам-пам"
     else:
         return "Ритма нет. Пам парам"
 
-stih = "пара-ра-рам рам-пам-папам па-ра-па-дам"
-print(ritm_slogi(stih))
-stih = "ЕХАЛ грека через реку. Видит грека  "
-print(ritm_slogi(stih))
+poem = "пара-ра-рам рам-пам-папам па-ра-па-дам"
+print(rhythm(poem))
+poem = "ЕХАЛ грека через реку. Видит грека  "
+print(rhythm(poem))
