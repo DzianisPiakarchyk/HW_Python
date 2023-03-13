023e6a8ca74a04359daaf085dc68608cc8a9f438
DzianisPiakarchyk <120118441+DzianisPiakarchyk@users.noreply.github.com>
2023-03-14 00:54:45 +0300

    Update t1.py

--- hw_7/t1.py
+++ hw_7/t1.py
@@ -11,10 +11,19 @@ def rhythm(poems):
         vowels.append(same_vowels)
     if len(set(vowels)) == 1:
         return "Ритм есть. Парам пам-пам"
-    else:
-        return "Ритма нет. Пам парам"
+    return "Ритма нет. Пам парам"
 
 poem = "пара-ра-рам рам-пам-папам па-ра-па-дам"
 print(rhythm(poem))
 poem = "ЕХАЛ грека через реку. Видит грека  "
 print(rhythm(poem))
+
+
+# def rifma(words):
+#     return len(set(map(lambda x: sum(1 for i in x if i in 'ёуеыаоэяию'), words))) == 1
+
+# chant = str(input('Введите текст: ')).lower().split()
+# if rifma(chant):
+#     print('Ритм есть. Парам пам-пам')
+# else:
+#     print('Ритма нет. Пам парам')
